cd7e1de97cd21c5e772b73cd8ee50e4d478a134d
Administrator <1163056678@qq.com>
2019-08-01 16:22:55 +0800

    springboot 添加过滤器 与 异步任务

---
+++ service.js
@@ -0,0 +1,64 @@
+let http = require('http');
+let url = require('url');
+let util = require('util');
+let fs = require('fs'); 
+
+var mine={
+    "css": "text/css",
+    "gif": "image/gif",
+    "html": "text/html",
+    "ico": "image/x-icon",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "js": "text/javascript",
+    "json": "application/json",
+    "pdf": "application/pdf",
+    "png": "image/png",
+    "svg": "image/svg+xml",
+    "swf": "application/x-shockwave-flash",
+    "tiff": "image/tiff",
+    "txt": "text/plain",
+    "wav": "audio/x-wav",
+    "wma": "audio/x-ms-wma",
+    "wmv": "video/x-ms-wmv",
+    "xml": "text/xml"
+};
+var path=require('path');
+
+var server = http.createServer(function (request, response) {
+    var pathname = url.parse(request.url).pathname;
+	
+    var realPath = path.join("./src/main/resources/META-INF/resources", pathname);    //这里设置自己的文件名称;   ./是路径可以自己配置
+
+    var ext = path.extname(realPath);
+    ext = ext ? ext.slice(1) : 'unknown';
+    fs.exists(realPath, function (exists) {
+        if (!exists) {
+            response.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
+
+            response.write("This request URL " + pathname + " was not found on this server.");
+            response.end();
+        } else {
+            fs.readFile(realPath, "binary", function (err, file) {
+                if (err) {
+                    response.writeHead(500, {
+                        'Content-Type': 'text/plain'
+                    });
+                    response.end(err);
+                } else {
+                    var contentType = mine[ext] || "text/plain";
+                    response.writeHead(200, {
+                        'Content-Type': contentType
+                    });
+                    response.write(file, "binary");
+                    response.end();
+                }
+            });
+        }
+    });
+});
+server.listen(3000,'127.0.0.1', ()=>{
+    console.log("服务器已经运行，请打开浏览,输入:http://127.0.0.1:3000/ 来进行访问.")
+  });
--- src/main/java/com/imooc/ImoocApplication.java
+++ src/main/java/com/imooc/ImoocApplication.java
@@ -1,9 +1,9 @@
 package com.imooc;
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
@@ -11,6 +11,10 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan(basePackages = ("com.imooc.mapper"))
 // 所有需要的包，包含一些工具类包，所在的路径
 @ComponentScan(basePackages = {"com.imooc","org.n3r.idworker"})
+// 开启定时扫描任务
+@EnableScheduling
+// 异步任务开启
+@EnableAsync
 public class ImoocApplication {
 
 	public static void main(String[] args) {
