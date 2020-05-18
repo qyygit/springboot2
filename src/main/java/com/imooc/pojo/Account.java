5b7bc51e18368d39dfea04d10687ccd90d69fee6
Mrs 洋 <tongyang0109@163.com>
2020-05-18 15:04:00 +0800

    测试自动生成pojo类

---
+++ src/main/java/com/imooc/pojo/Account.java
@@ -0,0 +1,54 @@
+package com.imooc.pojo;
+
+import javax.persistence.*;
+
+public class Account {
+    @Id
+    private Integer id;
+
+    private String name;
+
+    private Float money;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return money
+     */
+    public Float getMoney() {
+        return money;
+    }
+
+    /**
+     * @param money
+     */
+    public void setMoney(Float money) {
+        this.money = money;
+    }
+}
